69e35867373a132673b0971f004ecccdcd9b052d
Sathishkumar Kumar <quic_satku@quicinc.com>
2024-12-12 13:15:22 +0530

    setup_qrdk.sh: Update setup_qrdk.sh script
    Change-Id: Id5f6c559de5636c3e3339667ab8b1ae4e6c9e553
    Signed-off-by: Sathishkumar Kumar <quic_satku@quicinc.com>

--- scripts/setup_qrdk.sh
+++ scripts/setup_qrdk.sh
@@ -40,7 +40,7 @@ echo "Found $(lsb_release -d | awk -F: '{print $2}') OS installed on this system
 
 dpkg --list > ${logdir}/pkgs_found_before.log 2>&1
 
-apt-get -y install make gcc g++ diffstat texinfo chrpath bc gcc-multilib git gawk build-essential autoconf libtool libncurses-dev gettext gperf lib32z1 libc6-i386 g++-multilib python3-git bison
+apt-get -y install make gcc g++ binutils patch bzip2 flex gettext pkg-config unzip zlib1g-dev libc6-dev subversion libncurses5-dev gawk sharutils curl libxml-parser-perl ocaml-nox ocaml ocaml-findlib python-yaml libssl-dev libfdt-dev bison texi2html diffstat texinfo chrpath bc gcc-multilib git build-essential autoconf libtool libncurses-dev gperf lib32z1 libc6-i386 g++-multilib python-git coccinelle cproto zstd liblz4-tool dos2unix
 
 #coccienlle 1.1.1 needs to be installed from source code
 if [[ $(lsb_release -rs) != "22.04" ]]; then
